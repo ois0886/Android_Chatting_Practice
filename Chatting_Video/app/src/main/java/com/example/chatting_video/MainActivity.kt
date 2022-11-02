1437c086ee2280e6bf5c1da828aca713f9519a85
oh in seong <superohinsung@naver.com>
2022-11-02 21:03:39 +0900

    2강까지 수강

--- Chatting_Video/app/src/main/java/com/example/chatting_video/MainActivity.kt
+++ Chatting_Video/app/src/main/java/com/example/chatting_video/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.chatting_video
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatting_video.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButtonMain.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
