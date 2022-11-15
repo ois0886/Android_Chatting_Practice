faa42d039d7f955c65805e5350164d87a8a78335
oh in seong <superohinsung@naver.com>
2022-11-16 02:20:40 +0900

    로그인 및 메인액티비티 수정

--- Chatting_Video/app/src/main/java/com/example/chatting_video/LoginActivity.kt
+++ Chatting_Video/app/src/main/java/com/example/chatting_video/LoginActivity.kt
@@ -2,7 +2,6 @@ package com.example.chatting_video
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- Chatting_Video/app/src/main/java/com/example/chatting_video/MainActivity.kt
+++ Chatting_Video/app/src/main/java/com/example/chatting_video/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chatting_video.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     private lateinit var binding: ActivityMainBinding
 
@@ -14,6 +17,17 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        auth.createUserWithEmailAndPassword("abc", "123123")
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                } else {
+                }
+            }
+
 
         binding.loginButtonMain.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
